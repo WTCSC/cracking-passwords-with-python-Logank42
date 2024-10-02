725e5daaf317aff7ab8976e5056e9afa2ddf6fa0
Logan-Kirkpatrick <logan_kirkpatrick@icloud.com>
2024-10-02 15:46:34 -0600

    update

--- jill.py
+++ jill.py
@@ -22,44 +22,30 @@ def main():
 
         passworD = password.strip().split(':')
 
-        #print(passworD)
+        print(passworD)
         
 
 
-        # password[0] => username, password[1] => hashed password
-
-        username = []
-
-        username.append(passworD[0])
-
-        passWord = []
-
-        passWord.append(passworD[1])
-
-        currentpassword = []
-
-        for wordlist in wordlistfile.readlines():
-
-            #wordlistArr.append(word.strip())
-            currentpassword = wordlist.encode()
-            
-            sha256_hash = hashlib.sha256()
-
-            sha256_hash.update(currentpassword)
-
-            currentPassword = sha256_hash.hexdigest()
-
-            currentpassword.append(currentPassword)
-
-        if currentpassword == password[1]:
-            print(username, passWord)
-
+    # password[0] => username, password[1] => hashed password
+    username = []
+    username.append(passworD[0])
+    passWord = []
+    passWord.append(passworD[1])
+    finalpassword = []
+    for wordlist in wordlistfile.readlines():
+        #wordlistArr.append(word.strip())
+        currentpassword = wordlist.encode()
         
-
-            # hashwordlistArr.append(currentpassword)
-
-            
-             #   print(password)
-
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(currentpassword)
+        currentPassword = sha256_hash.hexdigest()
+        finalpassword.append(currentPassword)
+    if currentpassword == passworD[1]:
+        pass   
+    
+        # hashwordlistArr.append(currentpassword)
+        
+         #   print(password)
+    print(finalpassword)
 if __name__ == "__main__":
-                    main()
+    main()
